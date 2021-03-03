daac2d14ac0901b67575c9f9c2d2c8267863ed01
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-03-04 01:01:25 +0530

    updates: added image url field, comment and author schemas and blog page

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -2,26 +2,30 @@ const express = require('express');
 const Article = require('../models/articleModel');
 const router = express();
 
+// new article
 router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() });
 });
 
+// find article by id
 router.get('/:id', async (req, res) => {
   const article = await Article.findById(req.params.id);
   if (article == null) res.redirect('/');
   res.render('articles/show', { article: article });
 });
 
+// posting new article
 router.post('/', async (req, res) => {
   let article = new Article({
     title: req.body.title,
+    image: req.body.image,
     description: req.body.description,
     markdown: req.body.markdown,
   });
   try {
     await article.save();
     res.redirect(`/articles/${article.id}`);
-  } catch (e) {
+  } catch (err) {
     res.render('articles/new', { article: article });
   }
 });
