465b0b5fbed5c4296fcf924c69bc52943b8322c7
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-03-07 15:40:28 +0530

    updates: edit route working perfectly

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Article = require('../models/articleModel');
 const router = express();
 
-// new article
+// rendering new route
 router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() });
 });
@@ -30,6 +30,25 @@ router.post('/', async (req, res) => {
   }
 })
 
+// rendering edit route
+router.get('/:id/edit', (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('articles/edit', {article: article})
+  })
+})
+
+// edit route
+router.put('/:id', (req, res) => {
+  Article.findByIdAndUpdate(req.params.id, req.body, (err, article) => {
+    if (err) {
+      res.redirect('/');
+    } else {
+      res.redirect(`/articles/${req.params.id}`)
+    }
+  })
+})
+
+// delete route
 router.delete('/:id', async (req, res) => {
   await Article.findByIdAndDelete(req.params.id)
   res.redirect('/')
